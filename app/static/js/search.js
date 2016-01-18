26d4e4e5bf9c6d461c3259243bcc9dcb27c4a6a1
xl <26442779@qq.com>
2016-01-18 23:21:56 +0800

    search empty to reverse

--- app/static/js/search.js
+++ app/static/js/search.js
@@ -2,9 +2,10 @@
 var searchFunc = function(dataname, inputSearch) {
   //recieve an list of data indexes
   //color the corresponding nodes
-
-
-
+  if (dataname==""){
+    d3.selectAll('circle').style('opacity', 1)
+    d3.selectAll('line').style('opacity', 1)
+  }else{
   //retrive the indexes of nodes which contain the searched data
   $.getJSON('/mapperJsonSaved', function(data) {
     var nameIndexMap = data['nameIndexMap']
@@ -39,57 +40,8 @@ var searchFunc = function(dataname, inputSearch) {
       }
     }
 
-
-
-
-
-
-    /*
-    var exsitIndexes = [];
-    for (d in dataname){
-      for (i in nodes){
-        var members = nodes[i]['members'];
-        var flag = $.inArray(dataname[d], members);
-        if (flag != -1){
-          var theNode = nodes[i]['index']
-          exsitIndexes.push(theNode);
-        };
-      };
-    }
-    //remove duplicates
-    exsitIndexes = exsitIndexes.reverse()
-                                .filter(function (e, i, arr) {
-                                    return arr.indexOf(e, i+1) === -1;
-                                  }).reverse();
-
-    var modExsitIndexes = [];
-    for (i in exsitIndexes){
-      var eid = 'circleid_'+exsitIndexes[i];
-      modExsitIndexes.push(eid);}
-
-    var svg = d3.select('#graph');
-    var noneExsitIndexes = [];
-    svg.selectAll('circle')
-       .each(function(d,i){
-         //find the id of node which does NOT contain the search data
-         var flag = $.inArray(this.id, modExsitIndexes);
-         if (flag == -1){
-           noneExsitIndexes.push(this.id)
-         }
-       })
-    //do the coloring!
-    for (i in modExsitIndexes){
-      svg.select('#'+modExsitIndexes[i])
-         //.transition()
-         .style('opacity', 1)
-    };
-    for (i in noneExsitIndexes){
-      svg.select('#'+noneExsitIndexes[i])
-         //.transition()
-         .style('opacity', 0.1)
-    }
-    */
   })
+}
 }
 
 
