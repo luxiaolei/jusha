a46b5fe63341213f38077b5195748ca533ef5001
xl <26442779@qq.com>
2016-01-26 16:39:39 +0800

    remove candles children

--- app/static/js/candlestick.js
+++ app/static/js/candlestick.js
@@ -1,6 +1,7 @@
 
 
 var genCandles = function(){
+  $('#candles').children().remove()
   var margin = {top: 20, right: 20, bottom: 30, left: 50},
           width = 960 - margin.left - margin.right,
           height = 500 - margin.top - margin.bottom;
