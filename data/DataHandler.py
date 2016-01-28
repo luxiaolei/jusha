41f03d97b61f09f6639f051f159249d1b2d5eaf0
xl <26442779@qq.com>
2016-01-28 23:18:13 +0800

    generates 8 personized features

--- data/DataHandler.py
+++ data/DataHandler.py
@@ -71,17 +71,106 @@ def genReportFilter(reportDate):
     df.to_csv('stock.csv',index=False)
     return df
 
-def genFs(df):
+class GenF:
     """
     Generates features for each ts data, includes:
-    1. distance to the lowest price previously, in price
-    2. distance to the hightest price previously, in price
-    3.  
+    1. Dis2low, distance to the lowest price previously, in price
+    2. Dis2high, distance to the hightest price previously, in price
+    3. VolumeM5, (Volumn- averageVolume_in_past_5_days)/Volume
+    4. VolumeVarianceM5, std of volume in past 5 dys.
+    5. PriceBandM5, MaxPrice - MinPrice in past 5dys. Positive for increase
+    6. inBandM5, -1 for close price below the band, 0 for in, 1 for above
+    7. Dis2M5, Close price distance to M5 price
+    8. PriceStability, number of trading-days whose priceband(high-low) inclues current days' close price
     """
-    pass
+    def __init__(self, df, verbose= True):
+        self.df = df
+        self.verbose = verbose
+
+    def StartGen(self):
+        """
+        Generate fetures
+        """
+        data = np.empty((self.df.shape[0], 8))
+        col = ['Dis2low', 'Dis2high', 'VolumeM5', 'VolumeVarianceM5',\
+                    'PriceBandM5', 'inBandM5', 'Dis2M5', 'PriceStability']
+        for i in self.df.index:
+            dfpart = df.ix[:i, :]
+            data[i,[0,1]] = self.__Dis2highlow(i, dfpart)
+            data[i,[2,3,4,5,6]] = self.__M5(i, dfpart)
+            data[i,7] = self.__PriceStability(i, dfpart)
+        newdf = pd.DataFrame(data, columns = col)
+        return self.df.join(newdf)
+
+
+
+    def __Dis2highlow(self, index, dfpart):
+        """
+        1. Dis2low, distance to the lowest price previously, in price
+        2. Dis2high, distance to the hightest price previously, in price
+        """
+        lowest = dfpart.Close.min()
+        hightest = dfpart.High.max()
+        Dis2high = hightest - dfpart.High[index]
+        Dis2low = dfpart.Low[index] - lowest
+        return (Dis2high, Dis2low)
+
+    def __M5(self, index, dfpart):
+        """
+        3. VolumeM5, (Volumn- averageVolume_in_past_5_days)/Volume
+        4. VolumeVarianceM5, std of volume in past 5 dys.
+        5. PriceBandM5, MaxPrice - MinPrice in past 5dys. Positive for increase
+        6. inBandM5, -1 for close price below the band, 0 for in, 1 for above
+        7. Dis2M5, Close price distance to M5 price
+        """
+        if index>= 5:
+            dfpast5 = dfpart.ix[index-5:index-1, :]
+            assert dfpast5.shape[0] == 5
+            Vmean, VolumeVarianceM5 = dfpast5.Volume.mean(), dfpast5.Volume.std()
+            VolumeM5 = dfpart.Volume[index] - Vmean
+            #5, 6
+            Hi, Lo = dfpast5.High.values, dfpast5.Low.values
+            MaxPrice, MinPrice = Hi.max(), Lo.min()
+            if Hi.argmax() >= Lo.argmin():
+                #increasing
+                PriceBandM5 = MaxPrice - MinPrice
+            else:
+                PriceBandM5 = MinPrice - MaxPrice
+            currentPrice = dfpart.Close[index]
+            if currentPrice <= MinPrice:
+                inBandM5 = -1
+            elif currentPrice >= MaxPrice:
+                inBandM5 = 1
+            else:
+                inBandM5 = 0
+            #7
+            Dis2M5 = currentPrice - dfpart.MA5[index]
+
+
+            return (VolumeM5, VolumeVarianceM5, PriceBandM5, inBandM5, Dis2M5)
+        else:
+            return (dfpart.Volume[index], 0, dfpart.Close[index]*.2, 0, 0)
+
+    def __PriceStability(self, index, dfpart):
+        """
+        8. PriceStability, number of trading-days whose priceband(high-low) inclues current days' close price
+        """
+        cp = dfpart.Close[index]
+        if index > 0:
+            dfpast = dfpart.ix[: index-1, :]
+            PriceStability = dfpast.ix[(dfpast.High> cp) &\
+                                       (dfpast.Low< cp)].shape[0]
+            return PriceStability
+        else:
+            return 0
+
+
+
 
 
 
 if __name__ == '__main__':
     reportDate = pd.read_csv('600005report.csv').ix[:,1].dropna()
     df = genReportFilter(reportDate)
+    newdf = GenF(df)
+    newdf = newdf.StartGen()
